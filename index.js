cdcfc3ec819d39410e609f390d897319f8fa8606
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-23 11:37:01 +0600

    get all product api create

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-require ('dotenv').config()
-const app = express()
+require('dotenv').config();
+const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const port =process.env.PORT|| 5000
-
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwy78.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('tools-house').collection('products');
+        
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+        
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
-  res.send('Tools House server is running')
-})
+  res.send('Tools House server is running');
+});
 
 app.listen(port, () => {
-  console.log(`Tools House app listening on port ${port}`)
-})
+  console.log(`Tools House app listening on port ${port}`);
+});
