14827905eafcb7f2d9d828d2b472cec975b8e8fd
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-26 12:24:31 +0600

    jwt token add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -37,7 +38,8 @@ async function run() {
         $set:user
       }
       const result = await usersCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      res.send({result,token});
     })
 
 
